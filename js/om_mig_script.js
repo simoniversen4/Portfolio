14b0cbf57f9ba01190997b4db7e6b494c70f4984
simonostergaard <iversen96c@gmail.com>
2023-03-11 19:17:09 +0100

    fixes & cases underside tilføjet

--- js/om_mig_script.js
+++ js/om_mig_script.js
@@ -1,3 +1,5 @@
+// gallery 2
+
 //bubblegallery - først funktionen der retunere objekt referencen til elementbyid (funktionen hedder _)
 function _(x){
     return document.getElementById(x);
@@ -6,16 +8,16 @@ function _(x){
 let ba, bi=0, intrvl;
 //bubblegallery - BCA = bubble content array - her findes det andet content
 let bca = [
-    '<img src="image/mellow-gallery.png" alt="" class="gallery-img">',
-        '<img src="image/barebells-gallery.png" alt="" class="gallery-img">',
-        '<img src="image/schouliving-gallery.png" alt="" class="gallery-img">',
-        '<img src="image/feiber-gallery.png" alt="" class="gallery-img">',
-        '<img src="image/aboutvintage-gallery.png" alt="" class="gallery-img">'
+    '<img src="../image/slide1-me.png" alt="" class="gallery-img2">',
+        '<img src="../image/image1.png" alt="" class="gallery-img2">',
+        '<img src="../image/image4.png" alt="" class="gallery-img2">',
+        '<img src="../image/image17.png" alt="" class="gallery-img2">',
+        '<img src="../image/image28.png" alt="" class="gallery-img2">'
 ];
 //denne her funktion bliver triggeret af funktionen under 
 function bubbles(bi){
 //det her fader vores content mellem siderne
-_("gallery-content").style.opacity = 0;
+_("gallery-content2").style.opacity = 0;
 //så laver vi et loop med alle boblerne og ændres deres bg farve imens
 for(let i=0; i < ba.length; i++){
     ba[i].style.background = "rgba(0,0,0,.1)";
@@ -24,8 +26,8 @@ for(let i=0; i < ba.length; i++){
 ba[bi].style.background ="rgb(77, 126, 69)";
 //sætter en delay på 300ms
 setTimeout(function(){
-    _("gallery-content").innerHTML = bca[bi];
-    _("gallery-content").style.opacity = 1; 
+    _("gallery-content2").innerHTML = bca[bi];
+    _("gallery-content2").style.opacity = 1; 
     }, 300);
 }
 //sætter en timer på, så den skifter hvert 5. sekund(hvis bi er samme værdi som ba så skal den reset og den stiger med én hver gang)
@@ -37,6 +39,6 @@ function bubbleSlide(){
     bubbles(bi);
 }
 window.addEventListener("load", function(){
-    ba = _("bubbles").children;
+    ba = _("bubbles2").children;
     intrvl = setInterval(bubbleSlide, 5000);
 });
