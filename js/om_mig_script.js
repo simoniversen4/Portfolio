907210509b8d4ac832769f911ddacbe45f5e5312
simonostergaard <iversen96c@gmail.com>
2023-05-05 15:24:08 +0200

    fixes

--- js/om_mig_script.js
+++ js/om_mig_script.js
@@ -1,12 +1,9 @@
-// gallery 2
-
-//bubblegallery - først funktionen der retunere objekt referencen til elementbyid (funktionen hedder _)
 function _(x){
     return document.getElementById(x);
 }
-//bubblegallery - variabler for array, bubble index og setinterval variabler
+
 let ba, bi=0, intrvl;
-//bubblegallery - BCA = bubble content array - her findes det andet content
+
 let bca = [
     '<img src="../image/slide1-me.png" alt="" class="gallery-img2">',
         '<img src="../image/image1.png" alt="" class="gallery-img2">',
@@ -14,27 +11,27 @@ let bca = [
         '<img src="../image/image17.png" alt="" class="gallery-img2">',
         '<img src="../image/image28.png" alt="" class="gallery-img2">'
 ];
-//denne her funktion bliver triggeret af funktionen under 
+
 function bubbles(bi){
-//det her fader vores content mellem siderne
+
 _("gallery-content2").style.opacity = 0;
-//så laver vi et loop med alle boblerne og ændres deres bg farve imens
+
 for(let i=0; i < ba.length; i++){
     ba[i].style.background = "rgba(0,0,0,.1)";
 }
-//her sørger vi for at den valgte bobbles farve er blå
+
 ba[bi].style.background ="rgb(77, 126, 69)";
-//sætter en delay på 300ms
+
 setTimeout(function(){
     _("gallery-content2").innerHTML = bca[bi];
     _("gallery-content2").style.opacity = 1; 
     }, 300);
 }
-//sætter en timer på, så den skifter hvert 5. sekund(hvis bi er samme værdi som ba så skal den reset og den stiger med én hver gang)
+
 function bubbleSlide(){
     bi++;
     if(bi == ba.length){
-      bi = 0; //så her resetter den altså, så bubble index er 0 og loopet starter igen
+      bi = 0;
     }
     bubbles(bi);
 }
